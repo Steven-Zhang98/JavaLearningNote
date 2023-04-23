9f4706077562a4ddc742a89a21a32e82b9a88a69
鹿游原 <115378528+Steven-Zhang98@users.noreply.github.com>
2023-04-23 10:17:43 +0100

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,16 @@
+public class Student {
+   String name;
+   int age;
+
+    public  void sayHello(String name) {
+        System.out.println("局部变量:" + name);
+        System.out.println("成员变量:" + this.name);
+    }
+
+    public static void main(String[] args) {
+        Student stu = new Student();
+        stu.name = "钢门吹雪";
+        stu.age = 23;
+        stu.sayHello("西域狂鸭");
+    }
+}
